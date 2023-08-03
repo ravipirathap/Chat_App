a1e189ca141c8aa6796cbd1d0da413eb4852204c
“ravipirathap” <ravipirathap0715@gmail.com>
2023-08-03 16:51:42 +0530

    path add

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
+const path = require("path")
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
